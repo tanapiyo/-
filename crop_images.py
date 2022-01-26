d178387c4546075577b27956ec9d3cc2fc388ccd
tanabe ayaka <tanabe.ayaka@plus-zero.co.jp>
2022-01-26 17:46:03 +0900

    add crop image

--- crop_images.py
+++ crop_images.py
@@ -2,7 +2,15 @@ from pathlib import Path
 from PIL import Image
 
 
-def _crop_center(pil_img, crop_width, crop_height):
+def get_paths(input_dir, exts=None):
+    paths = sorted([x for x in input_dir.iterdir()])
+    if exts:
+        paths = list(filter(lambda x: x.suffix in exts, paths))
+
+    return paths
+
+
+def crop_center(pil_img, crop_width, crop_height):
     img_width, img_height = pil_img.size
     return pil_img.crop(((img_width - crop_width) // 2,
                          (img_height - crop_height) // 2,
@@ -11,21 +19,25 @@ def _crop_center(pil_img, crop_width, crop_height):
 
 
 def list_dirs_and_crop(folder):
+    print("processing" + folder + "images")
     input_dir = Path(r"./imgs/{folder}".format(folder=folder))
     # 出力先のディレクトリを作成する。
     output_dir = Path(r"./imgs/cropped_{folder}".format(folder=folder))
     output_dir.mkdir(exist_ok=True)
     # 画像ファイルの中を再起的に検索する
     for path in get_paths(input_dir, exts=[".jpg", ".jpeg", ".png"]):
+        print("crop target : " + str(path))
         im = Image.open(str(path))
-        _crop_center(im, 240, 240)
+        im = crop_center(im, 220, 220)
+        target_path = str(path).split("/")[2]
+        im.save(
+            "./imgs/cropped_{folder}".format(folder=folder) + '/' + target_path, quality=95)
 
 
-if __name__ == 'main':
+input_dir = Path(r"./imgs/")
+folders = ["leopard_200", "okonomiyaki_200",
+           "takoyaki_50", "takoyaki_300", "tiger_200"]
+for folder in folders:
+    list_dirs_and_crop(folder)
 
-    input_dir = Path(r"./imgs/")
-    for path in get_paths(input_dir, exts=[".jpg", ".jpeg", ".png"]):
-        print(path)
-
-    # 出力先のディレクトリを作成する
-    # 画像を読み込む
+# imgs/takoyaki_300/000281.jpg
